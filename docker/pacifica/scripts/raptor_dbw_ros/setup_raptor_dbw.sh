e1ccee9e194d4a8d4a6db4856d83aa52e7574512
Boluwatife Olabiran <bso19a@fsu.edu>
2024-03-23 13:01:58 -0400

    Updated autoware.repos to point to pacifica packages.

--- docker/pacifica/scripts/raptor_dbw_ros/setup_raptor_dbw.sh
+++ docker/pacifica/scripts/raptor_dbw_ros/setup_raptor_dbw.sh
@@ -6,10 +6,10 @@ sudo apt-add-repository -y ppa:astuff/kvaser-linux
 sudo apt update
 sudo apt install -y kvaser-canlib-dev kvaser-drivers-dkms kvaser-linlib-dev
 
-sudo apt install apt-transport-https
+sudo apt install -y apt-transport-https
 sudo sh -c 'echo "deb [trusted=yes] https://s3.amazonaws.com/autonomoustuff-repo/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/autonomoustuff-public.list'
 sudo apt update
-sudo apt install ros-${ROS_DISTRO}-kvaser-interface
+sudo apt install -y ros-${ROS_DISTRO}-kvaser-interface
 
 #cd /f1tenth_ws/src
 #git clone https://github.com/astuff/kvaser_interface.git -b ros2_master
