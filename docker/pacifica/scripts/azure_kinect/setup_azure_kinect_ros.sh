17d934691f7dbc6934f5a5116933aac7bfe0d0c7
Boluwatife Olabiran <bso19a@fsu.edu>
2024-06-24 21:02:52 -0400

    Updated autoware.repos to point to pacifica packages.

--- docker/pacifica/scripts/azure_kinect/setup_azure_kinect_ros.sh
+++ docker/pacifica/scripts/azure_kinect/setup_azure_kinect_ros.sh
@@ -1,4 +1,4 @@
-sudo apt update && sudo apt install ros-humble-joint-state-publisher ros-humble-joint-state-publisher-gui
+sudo apt update && sudo apt install -y ros-humble-joint-state-publisher ros-humble-joint-state-publisher-gui
 
 #AZURE_KINECT_ROS_REPO_CLONE="git clone https://github.com/privvyledge/Azure_Kinect_ROS_Driver.git -b humble"
 #AZURE_KINECT_ROS_REPO_CLONE="git clone https://github.com/logivations/Azure_Kinect_ROS_Driver.git -b humble_release-13.1"
